a4cb8da4f9189b9119084953a29446f6e1b42b2a
alx1992 <alx19920215@163.com>
2018-07-01 22:12:03 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,41 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * @author LiXin
+ * @date 2018/7/1 21:57
+ * City
+ */
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
