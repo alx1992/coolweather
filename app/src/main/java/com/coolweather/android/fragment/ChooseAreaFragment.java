abe7482d13ae203acd98666f3666ba0c8fa84d21
alx1992 <alx19920215@163.com>
2018-07-08 22:01:42 +0800

    加入显示天气信息的功功能

--- app/src/main/java/com/coolweather/android/fragment/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/fragment/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.coolweather.android.fragment;
 
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -16,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.coolweather.android.R;
+import com.coolweather.android.WeatherActivity;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
@@ -92,7 +94,7 @@ public class ChooseAreaFragment extends Fragment {
         listView.setAdapter(adapter);
         return view;
     }
-    
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -106,6 +108,12 @@ public class ChooseAreaFragment extends Fragment {
                 if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryConties();
+                }else if (currentLevel == LEVEL_COUNTY) {
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
 
             }
